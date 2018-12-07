b6248e02a967a1bef96c0fe9740b0756573ed250
MelodyV24 <MelodyV24@163.com>
2018-12-07 14:31:54 +0800

    更新数据

--- 数据处理/CNN/test_example_CNN.m
+++ 数据处理/CNN/test_example_CNN.m
@@ -7,20 +7,20 @@ clc
 % train_y = double(train_y');
 % test_y = double(test_y');
 
- load E:\毕业相关\大论文\数据处理\数据与结果\管道粘贴FBG\15Mpa\MapSize\10\data  %数据
- load E:\毕业相关\大论文\数据处理\数据与结果\管道粘贴FBG\15Mpa\MapSize\10\label %标签
+ load F:\毕业相关\大论文\GraduateWork\数据处理\数据与结果\直管\9Mpa\8\data  %数据
+ load F:\毕业相关\大论文\GraduateWork\数据处理\数据与结果\直管\9Mpa\8\label %标签
 %  l1=zeros(525,1);l2=ones(525,1);
 %  label=[l2 l1;l1 l2];
 %  data=data([1:525 3151:3675],:);
 %   data=data([1:1050 1576:2100 3151:3675],:);
-  data=data([1:840 1261:1680 2521:2940],:);
-  l1=zeros(420,1);l2=ones(420,1);
- label=[l2 l1 l1 l1;l1 l2 l1 l1;l1 l1 l2 l1;l1 l1 l1 l2;];
+   data=data([1:1750 3501:10500],:);
+   l1=zeros(1750,1);l2=ones(1750,1);
+  label=[l2 l1 l1 l1 l1;l1 l2 l1 l1 l1;l1 l1 l2 l1 l1;l1 l1 l1 l2 l1;l1 l1 l1 l1 l2;];
  Err=[];
  trainerr=[];
 %%定义好训练集与测试集数量
 rowNum=size(data,1);
-trainSize=1350;  %训练集数量
+trainSize=6000;  %训练集数量
 testSize=rowNum-trainSize;%测试集数量
 k=rand(1,rowNum);
 [m,n]=sort(k);
@@ -30,8 +30,8 @@ train_label=label(n(1:trainSize),:);
 test_data=data(n(trainSize+1:rowNum),:);
 test_label=label(n(trainSize+1:rowNum),:);
 %%转成待训练形势 三维数组 14*2000*行数
-train_x = double(reshape(train_data',10,10,trainSize))/255;
-test_x = double(reshape(test_data',10,10,testSize))/255;
+train_x = double(reshape(train_data',8,8,trainSize))/255;
+test_x = double(reshape(test_data',8,8,testSize))/255;
 train_y = double(train_label');
 test_y = double(test_label');
 
@@ -41,14 +41,14 @@ test_y = double(test_label');
 rand('state',0)
 cnn.layers = {
        struct('type', 'i') %input layer
-       struct('type', 'c', 'outputmaps', 4, 'kernelsize', 3) %convolution layer
+       struct('type', 'c', 'outputmaps', 3, 'kernelsize', 3) %convolution layer
        struct('type', 's', 'scale', 2) %sub sampling layer
-        struct('type', 'c', 'outputmaps',4, 'kernelsize', 3) %convolution layer
+         struct('type', 'c', 'outputmaps',3, 'kernelsize', 3) %convolution layer
 %         struct('type', 's', 'scale', 2) %subsampling layer
 };
 opts.alpha = 1;
-opts.batchsize = 25;%之前是144
-opts.numepochs = 3000;
+opts.batchsize = 1000;%之前是144
+opts.numepochs = 2000;
 cnn = cnnsetup(cnn, train_x, train_y);
 % [Err cnn] = cnntrain(cnn, train_x, train_y, opts,test_x,test_y);
 [Err,cnn,Output_shiji,Output_qiwang] = cnntrain(cnn, train_x, train_y, opts,test_x,test_y);
--- 数据处理/数据处理代码/LooseLevel_grayPhoto.m
+++ 数据处理/数据处理代码/LooseLevel_grayPhoto.m
@@ -1,7 +1,7 @@
 clc
 clear all
 
-path = 'E:\毕业相关\大论文\实验数据\弯管\20181122卡箍松动程度FBG\FBG解调系统数据文件\数据文件\2018-11-22\15Mpa';
+path = 'F:\毕业相关\大论文\GraduateWork\实验数据\直管\FBG解调系统数据文件\数据文件\2018-12-06\9Mpa';
 nx=dir(path);%读取盖路径下的所有文件
 nxx=nx(3:end);%去掉两个隐藏文件
 n=length(nxx);%该路径下txt文件个数
@@ -16,24 +16,23 @@ for i=1:n
   for j=1:n1
     str=strcat(str1,'\',nxx1(j).name);
       disp(str);
-    [~,~,~,~,~,F1,G1,H1,I1,J1,K1,L1,M1,N1,O1,P1]=textread(str,'%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f',14000,'headerlines',3); %跳过3行开始读
+    [~,~,~,~,~,F1,G1,H1,I1,J1,K1,L1,M1,N1]=textread(str,'%f%f%f%f%f%f%f%f%f%f%f%f%f%f',108000,'headerlines',3); %跳过3行开始读
 %     HH=1000*detrend(H1)/1.2;II=1000*detrend(I1)/1.2;JJ=1000*detrend(J1)/1.2;%通道2
 %     KK=1000*detrend(K1)/1.2;LL=1000*detrend(L1)/1.2;MM=1000*detrend(M1)/1.2;%通道3
 %     NN=1000*detrend(N1)/1.2;OO=1000*detrend(O1)/1.2;PP=1000*detrend(P1)/1.2;QQ=1000*detrend(Q1)/1.2;%通道1
 
-     AA(:,1)=1000*detrend(M1)/1.2;AA(:,2)=1000*detrend(N1)/1.2;AA(:,3)=1000*detrend(O1)/1.2;AA(:,4)=1000*detrend(P1)/1.2;
-     AA(:,5)=1000*detrend(G1)/1.2;AA(:,6)=1000*detrend(H1)/1.2;AA(:,7)=1000*detrend(I1)/1.2;
-     AA(:,8)=1000*detrend(J1)/1.2;AA(:,9)=1000*detrend(K1)/1.2;AA(:,10)=1000*detrend(L1)/1.2;%适用12,15Mpa
+%      AA(:,1)=1000*detrend(M1)/1.2;AA(:,2)=1000*detrend(N1)/1.2;AA(:,3)=1000*detrend(O1)/1.2;AA(:,4)=1000*detrend(P1)/1.2;
+%      AA(:,5)=1000*detrend(G1)/1.2;AA(:,6)=1000*detrend(H1)/1.2;AA(:,7)=1000*detrend(I1)/1.2;
+%      AA(:,8)=1000*detrend(J1)/1.2;AA(:,9)=1000*detrend(K1)/1.2;AA(:,10)=1000*detrend(L1)/1.2;%适用12,15Mpa
      
 %      AA(:,1)=1000*detrend(N1)/1.2;AA(:,2)=1000*detrend(O1)/1.2;AA(:,3)=1000*detrend(P1)/1.2;AA(:,4)=1000*detrend(Q1)/1.2;
 %      AA(:,5)=1000*detrend(H1)/1.2;AA(:,6)=1000*detrend(I1)/1.2;AA(:,7)=1000*detrend(J1)/1.2;
 %      AA(:,8)=1000*detrend(K1)/1.2;AA(:,9)=1000*detrend(L1)/1.2;AA(:,10)=1000*detrend(M1)/1.2;
      
-%      AA(:,1)=1000*detrend(N1)/1.2;AA(:,2)=1000*detrend(O1)/1.2;AA(:,3)=1000*detrend(P1)/1.2;AA(:,4)=1000*detrend(Q1)/1.2;
-%      AA(:,5)=1000*detrend(H1)/1.2;AA(:,6)=1000*detrend(J1)/1.2;
-%      AA(:,7)=1000*detrend(K1)/1.2;AA(:,8)=1000*detrend(L1)/1.2;AA(:,9)=1000*detrend(M1)/1.2;%9个点
-        for j=1:140
-            I=reshape(AA((1+(j-1)*100):(100*j))',1,10*10);
+     AA(:,1)=1000*detrend(G1)/1.2;AA(:,2)=1000*detrend(H1)/1.2;AA(:,3)=1000*detrend(I1)/1.2;AA(:,4)=1000*detrend(J1)/1.2;AA(:,5)=1000*detrend(K1)/1.2;
+     AA(:,6)=1000*detrend(L1)/1.2;AA(:,7)=1000*detrend(M1)/1.2;AA(:,8)=1000*detrend(N1)/1.2;%直管的
+        for j=1:270
+            I=reshape(AA((1+(j-1)*400):(400*j))',1,8*50);
             data_temp=mapminmax(I, 0, 255);
             data=[data;data_temp];
         end
@@ -45,7 +44,7 @@ for i=1:n
       subplot(7,1,i);plot(A);
 end
 
-l1=zeros(420,1);l2=ones(420,1);
-label=[l2 l1 l1 l1 l1 l1 l1;l1 l2 l1 l1 l1 l1 l1;l1 l1 l2 l1 l1 l1 l1;l1 l1 l1 l2 l1 l1 l1;;l1 l1 l1 l1 l2 l1 l1;l1 l1 l1 l1 l1 l2 l1;;l1 l1 l1 l1 l1 l1 l2];
+l1=zeros(270,1);l2=ones(270,1);
+label=[l2 l1 l1 l1 l1 l1;l1 l2 l1 l1 l1 l1;l1 l1 l2 l1 l1 l1;l1 l1 l1 l2 l1 l1;l1 l1 l1 l1 l2 l1;l1 l1 l1 l1 l1 l2];
 save data data;
 save label label;
